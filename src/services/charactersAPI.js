09b437d705addb85063d4a6a41e9f41e4d38ab13
jhonatavbrg <jhonatavbrg@gmail.com>
2021-11-18 02:00:43 -0300

    fix function request api and timeout config

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,13 +1,7 @@
 import axios from 'axios';
 
-require('dotenv').config();
-
-const { REACT_APP_HAWKINS_URL,
-  REACT_APP_HAWKINS_TIMEOUT } = process.env;
-
-const DEFAULT_TIMEOUT = REACT_APP_HAWKINS_TIMEOUT;
 class CharactersService {
-  constructor({ url = REACT_APP_HAWKINS_URL, timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url, timeout }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
