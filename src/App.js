1e8657624c64da7ed143ad17946ea98e7ffdd9d1
jhonatavbrg <jhonatavbrg@gmail.com>
2021-11-18 13:12:50 -0300

    fix all erros on heroku

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      { process.env.REACT_APP_ENVIROMENT === 'development' && (
+        <div>Em desenvolvimento</div>
+      )}
       <StrangerThings />
     </div>
   );
